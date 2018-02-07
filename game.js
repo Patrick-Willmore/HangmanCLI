c7fceea21f4d0bbaea20596721cf048d15f43d37
Patrick Willmore <patman1127@gmail.com>
2018-02-07 13:35:47 -0700

    work w tutor

---
+++ game.js
@@ -0,0 +1,35 @@
+// runs inquirer and asks the user a series of questions whose replies are
+// stored within the variable answers inside of the .then statement.
+var inquirer = require("inquirer");
+var Word = require('./word.js');
+
+// list of words (array)
+// main game function
+
+function mainGame() {
+    //function beginGame
+        //set guesses remaining
+        //get next word
+    
+    //function word from list of words
+        //prompt user to guess ltr
+
+    function getGuess(){
+
+
+        inquirer.prompt([
+            {
+                name: "name",
+                message: "What letter do you choose?"
+            }
+        ]).then(function (answers) {
+            // initializes the variable newguy to be a programmer object which will take
+            // in all of the user's answers to the questions above
+            var newGuy = new Programmer(answers.name, answers.position, answers.age, answers.language);
+            // printInfo method is run to show that the newguy object was successfully created and filled
+            newGuy.printInfo();
+        });
+    }
+}
+
+
--- word.js
+++ word.js
@@ -1,61 +1,15 @@
-// dependency for inquirer npm package
-var inquirer = require("inquirer");
+// dependency for inquirer npm pack
+var Letter = require('./letter.js');
 
 // constructor function used to create programmers objects
-function Programmer(name) {
-  this.name = name;
-  // this.position = position;
-  // this.age = age;
-  // this.language = language;
+function Word(wordtoGuess) {
+  this.lettersArray =  wordtoGuess.split("").map(function(char){
+    return new Letter(char);
+  });
+
 }
 
 // creates the printInfo method and applies it to all programmer objects
 Programmer.prototype.printInfo = function() {
   console.log("Letter: " + this.name);
 };
-
-// runs inquirer and asks the user a series of questions whose replies are
-// stored within the variable answers inside of the .then statement.
-inquirer.prompt([
-  {
-    name: "name",
-    message: "What letter do you choose?"
-  }
-]).then(function(answers) {
-  // initializes the variable newguy to be a programmer object which will take
-  // in all of the user's answers to the questions above
-  var newGuy = new Programmer(answers.name, answers.position, answers.age, answers.language);
-  // printInfo method is run to show that the newguy object was successfully created and filled
-  newGuy.printInfo();
-});
-
-
-// var letter = require('./letter.js');
-
-// const inquirer = require('inquirer');
-
-// // userprompt();
-// function userprompt() {
-//     inquirer.prompt({
-            
-//             name: "userguess",
-//             type: "input",
-//             message: "Please input next letter guessed"
-            
-//     })
-//         .then(function(inquirerResponse){
-//         // userprompt();
-//             console.log(inquirerResponse);
-//         });
-//     };
-            // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
-        //     if (inquirerResponse.confirm) {
-        //       console.log("\nWelcome " + inquirerResponse.name);
-              
-        //     }
-        //     else {
-        //       console.log("\nThat's okay " + inquirerResponse.name + ", come again when you are more sure.\n");
-        //     }
-        //   });
-    
-        // userprompt();
